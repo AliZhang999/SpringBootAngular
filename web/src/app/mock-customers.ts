541487da6dddb579abf1905f068b511ee88505db
Aligithub185 <Alizhang999@hotmail.com>
2018-02-02 10:39:24 +0800

    update2

--- web/src/app/mock-customers.ts
+++ web/src/app/mock-customers.ts
@@ -5,5 +5,6 @@ export const CUSTOMERS: Customer[] = [
     new Customer(2, 'bb', 'BB', 2),
     new Customer(3, 'cc', 'CC', 3),
     new Customer(4, 'dd', 'DD', 4),
-    new Customer(5, 'ee', 'EE', 5)
+    new Customer(5, 'ee', 'EE', 5),
+    new Customer(6, 'ee', 'EE', 6),
 ];
